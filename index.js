b5a474e38b29d6f64b3ad1b36a7791873be48def
seejoshcode@gmail.com <seejoshcode@gmail.com>
2019-05-24 14:00:41 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  const navBar = document.getElementsByTagName("nav")[0];
+
+  if (window.sessionStorage.getItem("navAnimationCount") === null) {
+    navBar.classList.add("slide-down");
+    window.sessionStorage.setItem("navAnimationCount", 1);
+  } else if (window.sessionStorage.getItem("navAnimationCount") == 1) {
+    navBar.classList.add("nav-is-active");
+  }
+};
